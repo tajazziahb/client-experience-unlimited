33997c2fccfe3aa3291164791d5c4d2fc98df5d7
JazziB14 <bootht14@gmail.com>
2024-11-12 11:24:07 -0500

    Add styles for buttons and dropdown menus
    Updated `src/styles.css` to include styles for buttons, dropdown menus, and various utility classes. These changes aim to improve the visual coherence and interactivity of buttons and dropdown elements across the site. Additionally, created a new `contact/index.html` file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["index.html","./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'peach': '#ffd3ac',
+      'black': '#020617',
+      'white': '#ffffff'
+    },
     extend: {},
   },
   plugins: [require('daisyui'),],
