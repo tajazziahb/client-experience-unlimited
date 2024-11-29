76b9cc9f0f3d993f2f1d0255f04bc01b4d3e2ac3
JazziB14 <bootht14@gmail.com>
2024-11-29 11:59:47 -0500

    Update contact page and enhance CSS styling
    This commit revamps the contact page layout and updates styles with new CSS classes, tailored for better responsiveness and design consistency. It includes upgrades to DaisyUI and TailwindCSS to their latest versions for enhanced styling features and support. Additionally, a navigation menu has been added, and some elements in the `index.html` have been refined to match the brand's theme more effectively.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html","./src/**/*.{html,js}"],
+  content: ["./contact/index.html","index.html","./src/**/*.{html,js}"],
   theme: {
     colors: {
       'peach': '#ffd3ac',
