9e910813ee52d4d49b8f59831d2a84637381cff9
JazziB14 <bootht14@gmail.com>
2024-11-29 12:28:39 -0500

    Update Vite config to include Rollup multi-page setup
    Reconfigure the Vite build process to support multiple HTML pages by defining specific entry points for 'main' and 'contact' pages using Rollup options. This change optimizes the build setup for a multi-page application, ensuring proper bundling of resources for each page.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.js
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                contact: resolve(__dirname, 'contact/index.html'),
+
+            },
+        },
+    },
 })
